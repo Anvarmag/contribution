7fe2362e0f22f0fd990fa75cfc52d7b3c32ac1ae
Anvarmag <Anvarfon@gmail.com>
2021-02-17 20:38:49 +0300

    Main.java added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int percent = 3;
+        int sum = 1000;
+        int income = sum * percent/100;
+        System.out.println(income);
     }
 }
